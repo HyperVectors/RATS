62109ed20df3544c29ed9bba5a6037d5fdf50e2d
felixkern04 <felixkern04@gmail.com>
2025-07-10 15:45:19 +0200

    Added Resize augmenter (does not currently interpolate values)

--- fraug/src/augmenters/resize.rs
+++ fraug/src/augmenters/resize.rs
@@ -1,12 +1,15 @@
 use super::base::Augmenter;
 
 /// Changes temporal resolution of time series by changing the length
+/// 
+/// Does not interpolate values!
 pub struct Resize {
+    /// size after the augmentation
     size: usize
 }
 
 impl Resize {
-    /// Creates new drift augmenter
+    /// Creates new resize augmenter
     pub fn new(size: usize) -> Self {
         Resize { size }
     }
@@ -14,6 +17,33 @@ impl Resize {
 
 impl Augmenter for Resize {
     fn augment_one(&self, x: &[f64]) -> Vec<f64> {
-        Vec::new()
+        let ratio = x.len() as f64 / self.size as f64;
+        (0..self.size).map(|i| {x[(i as f64 * ratio) as usize]}).collect()
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn resize_smaller() {
+        let series = vec![1.0; 90].iter().enumerate().map(|(i, _)| i as f64).collect::<Vec<_>>();
+
+        let aug = Resize::new(10);
+        let series = aug.augment_one(&series);
+
+        assert_eq!(series, vec![0.0, 9.0, 18.0, 27.0, 36.0, 45.0, 54.0, 63.0, 72.0, 81.0]);
+    }
+
+    #[test]
+    fn resize_larger() {
+        let series = vec![1.0; 5].iter().enumerate().map(|(i, _)| i as f64).collect::<Vec<_>>();
+
+        let aug = Resize::new(12);
+        let series = aug.augment_one(&series);
+
+        assert_eq!(series, vec![0.0, 0.0, 0.0, 1.0, 1.0, 2.0, 2.0, 2.0, 3.0, 3.0, 4.0, 4.0]);
+    }
+
 }
