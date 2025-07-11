525ea49f87cd01dba5f4af10173c15794f72e8e4
felixkern04 <felixkern04@gmail.com>
2025-07-11 12:39:12 +0200

    Improved implementation of repeat

--- fraug/src/augmenters/repeat.rs
+++ fraug/src/augmenters/repeat.rs
@@ -20,21 +20,16 @@ impl Repeat {
 
 impl Augmenter for Repeat {
     fn augment_batch(&self, input: &mut Dataset, _parallel: bool) {
-        let mut new_features: Vec<Vec<f64>> = Vec::with_capacity(input.features.len() * self.n);
-        let mut new_labels: Vec<String> = Vec::with_capacity(input.labels.len() * self.n);
+        let features: Vec<Vec<f64>> = input.features.clone();
+        let labels: Vec<String> = input.labels.clone();
 
-        input.features.iter().enumerate().for_each(|(i, x)| {
-            for _ in 0..self.n {
-                new_features.push(x.clone());
-                new_labels.push(input.labels[i].clone());
-            }
-        });
-
-        input.features = new_features;
-        input.labels = new_labels;
+        for _ in 0..self.n-1 {
+            input.features.append(&mut features.clone());
+            input.labels.append(&mut labels.clone());
+        }
     }
 
     fn augment_one(&self, _x: &[f64]) -> Vec<f64> {
-        unimplemented!("Repeat augmenter only works on the dataset directly!");
+        unimplemented!("Repeat augmenter only works on a dataset directly!");
     }
 }
